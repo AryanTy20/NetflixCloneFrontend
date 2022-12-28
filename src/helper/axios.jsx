097c2ffc6ece6dcaf5a4514d654e219baaa6d029
Aryan Tirkey <65565216+AryanTy20@users.noreply.github.com>
2022-12-28 18:11:41 +0530

    api url updated

--- src/helper/axios.jsx
+++ src/helper/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const baseURL = "https://netflixclon3.herokuapp.com";
-const baseURL="https://netflixcloneapi.up.railway.app";
+// const baseURL="https://netflixcloneapi.up.railway.app";
+const baseURL="https://netflix-api-zh2w.onrender.com";
 // const baseURL = "http://localhost:8000";
 export const Axios = axios.create({
   baseURL,
